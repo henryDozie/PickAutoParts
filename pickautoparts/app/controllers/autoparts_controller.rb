c7327185385e232ad8cea18cfe48cc2ec878455b
henrydozie <hcewurum@gmail.com>
2020-02-11 13:46:24 -0500

    set up the autopart controller

--- pickautoparts/app/controllers/autoparts_controller.rb
+++ pickautoparts/app/controllers/autoparts_controller.rb
@@ -1,5 +1,6 @@
 class AutopartsController < ApplicationController
   before_action :set_order
+  # before_action :set_brand
   before_action :set_order_autopart, only: [:show, :update, :destroy]
 
   # GET /orders/:order_id/autoparts
@@ -18,37 +19,33 @@ class AutopartsController < ApplicationController
 
   # POST /orders/:order_id/autoparts
   def create
-    # if(set_order)
     @order.autoparts.create!(autopart_params)
-    # else
-    #   json_response(@brand.autoparts)
-    json_response(status: "SUCCESS", message: 'autopart created successfully.', data: @autopart.name)
-
+    json_response(status: "SUCCESS", message: 'autopart created successfully.')
   end
 
   # PUT /orders/:order_id/autoparts/:id
   def update
     @autopart.update(autopart_params)
-    json_response(status: 'SUCCESS', message: 'autopart updated successfully.', data: @autopart.name)
+    json_response(status: 'SUCCESS', message: 'autopart updated successfully.')
   end
 
   # DELETE /orders/:order_id/autoparts/:id
   def destroy
     @autopart.destroy
-    json_response(status: 'SUCCESS', message: 'autopart deleted successfully.', data: @autopart.name)
+    json_response(status: 'SUCCESS', message: 'autopart deleted successfully.')
   end
 
   private
 
-  def item_params
-    params.permit(:name, :done)
+  def autopart_params
+    params.permit(:name, :img_url, :description)
   end
 
   def set_order
     @order = Order.find(params[:order_id])
   end
 
-  def set_order_item
+  def set_order_autopart
     @autopart = @order.autoparts.find_by!(id: params[:id]) if @order
   end
 end
