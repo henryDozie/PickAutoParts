39bb867c791cb6bae4e5fb61b78e3d6e7c05cf5c
henrydozie <hcewurum@gmail.com>
2020-02-11 16:50:06 -0500

    Added more robust configuration to the routes

--- pickautoparts/config/routes.rb
+++ pickautoparts/config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :orders do
-    resources :autoparts do
-      resources :reviews
+  resources :users do
+    resources :orders do
+      resources :autoparts do
+        resources :reviews
+      end
     end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+
+  post 'password/forgot', to: 'password#forgot'
+  post 'password/reset', to: 'password#reset'
 
 end
