8727811fbdbbee18583ad7266df6203bc7b447f1
henrydozie <hcewurum@gmail.com>
2020-02-11 13:51:13 -0500

    added more routes for the reviews and autoparts

--- pickautoparts/config/routes.rb
+++ pickautoparts/config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :orders do
-    resources :autoparts
-  end
-
-  resources :brands do
-    resources :autoparts
+    resources :autoparts do
+      resources :reviews
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
