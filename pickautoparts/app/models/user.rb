68a74be53793036b550d69e27001a808b922a758
henrydozie <hcewurum@gmail.com>
2020-02-11 13:50:32 -0500

    updated the user model

--- pickautoparts/app/models/user.rb
+++ pickautoparts/app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   # encrypt password
   has_secure_password
 
-  # Model associations
+  # Model associationss
   has_many :orders, foreign_key: :created_by
+  # has_many :brands, foreign_key: :created_by
   # Validations
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password_digest, presence: true
+  validates :address, presence: true
 end
