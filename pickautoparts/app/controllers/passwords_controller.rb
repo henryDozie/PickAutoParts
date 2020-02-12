4bced5a632ff43cc92771cbff01024e565eb1131
henrydozie <hcewurum@gmail.com>
2020-02-12 09:42:51 -0500

    Refactored the password controller for resetting a password

--- pickautoparts/app/controllers/passwords_controller.rb
+++ pickautoparts/app/controllers/passwords_controller.rb
@@ -8,7 +8,7 @@ class PasswordsController < ApplicationController
 
     if user.present?
       user.generate_password_token! #generate pass token
-      # @user.email
+      UserMailer.reset_email(user).deliver
       render json: {status: 'ok'}, status: :ok
     else
       render json: {error: [’Email address not found. Please check and try again.’]}, status: :not_found
@@ -19,19 +19,19 @@ class PasswordsController < ApplicationController
     token = params[:token].to_s
 
     if params[:email].blank?
-      return render json: {error: 'Token not present’}
+      return render json: {error: 'Token not present'}
     end
 
     user = User.find_by(reset_password_token: token)
 
     if user.present? && user.password_token_valid?
       if user.reset_password!(params[:password])
-        render json: {status: 'ok’}, status: :ok
+        render json: {status: 'ok'}, status: :ok
       else
         render json: {error: user.errors.full_messages}, status: :unprocessable_entity
       end
     else
-      render json: {error:  [’Link not valid or expired. Try generating a new link.’]}, status: :not_found
+      render json: {error:  ['Link not valid or expired. Try generating a new link.']}, status: :not_found
     end
   end
 end
