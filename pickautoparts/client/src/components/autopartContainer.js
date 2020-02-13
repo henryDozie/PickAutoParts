69febb230bc30f2e85db942b35663beb3b089f9a
henrydozie <hcewurum@gmail.com>
2020-02-13 17:13:05 -0500

    created the container for the autoparts

---
+++ pickautoparts/client/src/components/autopartContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { indexAutoparts } from "../services/api_helper";
+
+class AutopartContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      autoparts: [],
+      apiLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    this.readAllAutoparts();
+    this.setState({
+      apiLoaded: true
+    });
+  }
+
+  readAllAutoparts = async () => {
+    const autoparts = await indexAutoparts();
+    this.setState({ autoparts });
+  };
+
+  render() {
+    return (
+      <div className="autos">
+        {this.state.apiLoaded &&
+          this.state.autoparts.map(autopart => (
+            <div className="autopartCard">
+              <div className="img">
+                <img src={autopart.img_url} alt="loading"></img>
+              </div>
+              <div className="partdesc">
+                <h3>{autopart.name}</h3>
+                <h5>{autopart.description}</h5>
+              </div>
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
+
+export default AutopartContainer;
