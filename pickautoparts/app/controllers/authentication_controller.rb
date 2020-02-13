84f3292f2e77b6dd8e011b3f7660ea92231c9d9f
henrydozie <hcewurum@gmail.com>
2020-02-13 17:02:54 -0500

    Added more flexibility for the user

--- pickautoparts/app/controllers/authentication_controller.rb
+++ pickautoparts/app/controllers/authentication_controller.rb
@@ -1,10 +1,12 @@
 class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
+
   # return auth token once user is authenticated
   def authenticate
     auth_token =
         AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-    json_response(auth_token: auth_token)
+    user = User.find_by(email: auth_params[:email])
+    json_response(user: user, auth_token: auth_token)
   end
 
   private
