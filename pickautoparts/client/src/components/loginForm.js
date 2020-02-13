78ad434309ed7d64ababf8555a5cc963db7c9530
henrydozie <hcewurum@gmail.com>
2020-02-13 17:07:12 -0500

    Refactore the login form

--- pickautoparts/client/src/components/loginForm.js
+++ pickautoparts/client/src/components/loginForm.js
@@ -1,42 +1,49 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 
-export default class LoginForm extends Component {
+class LoginForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       email: "",
       password: ""
-    }
+    };
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
-  }
+  };
 
   render() {
     return (
-      <form onSubmit={(e) => this.props.handleLogin(e, this.state)}>
-        <h2>Login!</h2>
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          name="email"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <button>Submit</button>
-        <Link to="/register">Register</Link>
-      </form>
-    )
+      <div className="loginPage">
+        <form
+          onSubmit={e => this.props.handleLogin(e, this.state)}
+          className="loginForm"
+        >
+          <h1>Login!</h1>
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            placeholder="Email"
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <button>Submit</button>
+          <Link to="/register">Register</Link>
+        </form>
+      </div>
+    );
   }
 }
+
+export default withRouter(LoginForm);
