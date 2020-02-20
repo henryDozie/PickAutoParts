4476460f49c0a61cf3a1911a252705ff2f111cea
henrydozie <hcewurum@gmail.com>
2020-02-20 09:34:54 -0500

    Updated API

--- pickautoparts/client/src/services/api_helper.js
+++ pickautoparts/client/src/services/api_helper.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  // baseURL: "http://localhost:3000"
+  baseURL: "https://afternoon-coast-26036.herokuapp.com/"
 })
 
 // AUTH
